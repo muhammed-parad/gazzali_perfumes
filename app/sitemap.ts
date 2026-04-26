f39feaae3a10f1d943e90f3cd6e77501025588bf
Muhammed Parad <muhammedparad0485@gmail.com>
2026-04-26 21:05:12 +0530

    Add shop URL to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,5 +6,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: 'https://gazzali-perfumes.vercel.app',
       lastModified: new Date(),
     },
+    {
+      url: 'https://gazzali-perfumes.vercel.app/shop',
+      lastModified: new Date(),
+    },
   ]
 }
